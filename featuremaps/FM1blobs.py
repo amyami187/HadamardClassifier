42f14f06b9c4dad037e36cd9781dec888a98c2b2
Amira <amyami187@gmail.com>
2020-01-10 18:10:06 +0200

    Create FM1blobs.py

---
+++ featuremaps/FM1blobs.py
@@ -0,0 +1,13 @@
+def featuremap(x1, x2, y, phi0):
+    # encode y label
+    if y == 1:
+        qml.CNOT(wires=[6, 5])  # flip label qubit
+    # feature map
+    qml.CRY(x1, wires=[6, 3])
+    qml.CRY(x2, wires=[6, 4])
+    qml.CNOT(wires=[3, 4])
+    qml.CNOT(wires=[4, 3])
+    qml.CRX(phi0[0], wires=[6, 3])
+    qml.CRX(phi0[0], wires=[6, 4])
+    qml.CNOT(wires=[3, 4])
+    qml.CNOT(wires=[4, 3])
